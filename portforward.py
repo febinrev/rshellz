b5d314c0b3a3527dd02855f7d148917d30469a3e
febinrev <52229330+febinrev@users.noreply.github.com>
2019-09-02 09:10:22 +0530

    Update portforward.py

--- portforward.py
+++ portforward.py
@@ -9,7 +9,18 @@ FEBREV-LINUXPLOIT
      coded By: Febin Rev
 """)
 port=input("ENTER THE PORT YOU USED IN THE PAYLOAD TO FORWARD OVER INTERNET : ")
-print(f"PORT FORWARDING ENABLED ON PORT >>> {port}")
-print("CTRL-C TO STOP,,,,,TERMINATING THIS CAN STOP PORT FORWARDING")
-os.system(f"ssh -R {port}:localhost:{port} serveo.net")
-
+print("[1] serveo.net       [2] localhost.run")
+vendor=click.prompt("Enter the portforwarding platform :", type=int, default=2)
+if vendor==1:
+          print(f"PORT FORWARDING ENABLED ON PORT >>> {port}")
+          print("CTRL-C TO STOP,,,,,TERMINATING THIS CAN STOP PORT FORWARDING")
+          os.system(f"ssh -R {port}:localhost:{port} serveo.net")
+elif vendor==2:
+          print(f"PORT FORWARDING ENABLED ON PORT >>> {port} ")
+          print("CTRL-C TO STOP,,,,TERMINATING THIS CAN STOP PORT FORWARDING")
+          os.system(f"ssh -R {port}:localhost:{port} localhost.run")
+else:
+          print("WRONG INPUT....going for default")
+          print(f"PORT FORWARDING ENABLED ON PORT >>> {port} ")
+          print("CTRL-C TO STOP,,,,TERMINATING THIS CAN STOP PORT FORWARDING")
+          os.system(f"ssh -R {port}:localhost:{port} localhost.run")
